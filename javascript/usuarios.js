ce878915e39cb3f810ec23126226355ea81c9f20
Felismino Chico <felisminochico35@gmail.com>
2025-02-27 12:42:59 +0000

    Últimos toques do dia de hoje

--- javascript/usuarios.js
+++ javascript/usuarios.js
@@ -5,7 +5,7 @@ let tagNav = this.document.getElementsByTagName('nav')[0];
 botaoMenu.addEventListener('click', mostrarFecharMenu)
 
 function mostrarFecharMenu(){
-    if(tagNav.style.marginLeft < '0px'){
+    if(tagNav.style.marginLeft < '0px'){        
         tagNav.style.marginLeft = '0px';
     }else{
         tagNav.style.marginLeft = '-300px'
