8b9ce06890885586b281d9c66f086dbbfd459836
Felismino Chico <felisminochico35@gmail.com>
2025-03-01 13:26:58 +0000

    Criação do efeito blur  e outras páginas

--- javascript/usuarios.js
+++ javascript/usuarios.js
@@ -1,13 +1,22 @@
 //alert("Olá, Mundo!");
 let botaoMenu = this.document.getElementById('btn-menu');
 let tagNav = this.document.getElementsByTagName('nav')[0];
+let tagMain = this.document.getElementsByTagName('main')[0];
 
 botaoMenu.addEventListener('click', mostrarFecharMenu)
+tagMain.addEventListener('click', fecharMenu)
 
 function mostrarFecharMenu(){
     if(tagNav.style.marginLeft < '0px'){        
         tagNav.style.marginLeft = '0px';
+        tagMain.style.filter = 'blur(7px)';
     }else{
-        tagNav.style.marginLeft = '-300px'
+        tagNav.style.marginLeft = '-300px';
+        tagMain.style.filter = 'blur(0px)';
     }
+}
+
+function fecharMenu(){
+    tagNav.style.marginLeft = '-300px';
+    tagMain.style.filter = 'blur(0px)';
 }
